d7be600a1104da4bbd34c2efe804fdfedc762880
thiomains <thiomains@icloud.com>
2025-07-26 19:57:35 +0200

    added logout path

---
+++ app/pages/logout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const session = useState('session')
+const config = useRuntimeConfig()
+
+onMounted(async () => {
+  try {
+    const res = await $fetch(config.public.apiBaseUrl + '/auth/logout', {
+      method: "POST",
+      credentials: "include"
+    })
+
+    session.value = {}
+
+    console.log(res)
+  } catch (e) {
+    const error = e as any;
+    console.log(error)
+  }
+
+})
+
+</script>
+
+<template>
+<p>Logging you out</p>
+</template>
+
+<style scoped>
+
+</style>
