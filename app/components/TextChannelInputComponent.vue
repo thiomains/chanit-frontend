7c0e3130e15485cc8344480f83452a4c5f36cdc9
thiomains <thiomains@icloud.com>
2025-07-28 01:41:25 +0200

    added sending messages to direct message channels

--- app/components/TextChannelInputComponent.vue
+++ app/components/TextChannelInputComponent.vue
@@ -1,12 +1,44 @@
-<script setup lang="ts">
-let message = ref("")
+<script setup>
+const config = useRuntimeConfig()
+const route = useRoute()
+const session = useState("session")
+
+const emit = defineEmits(['message-sent'])
+
+let messageInput = ref("")
+
+async function enterPressed() {
+  const messageBody = messageInput.value.trim()
+  if (messageBody.length === 0) {
+    return
+  }
+  try {
+    const res = await $fetch(config.public.apiBaseUrl + "/channel/" + route.params.id + "/messages", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${session.value.session.accessToken}`,
+        Session: session.value.session.sessionId
+      },
+      body: {
+        body: messageBody
+      }
+    })
+
+    emit("message-sent", res)
+
+    messageInput.value = ""
+  } catch (e) {
+    console.log(e)
+  }
+}
 </script>
 
 <template>
   <div class="p-4">
     <UInput
+        @keydown.enter="enterPressed"
         class="w-full"
-        v-model="message"
+        v-model="messageInput"
         placeholder="Type a message..."
         size="xl"
         :ui="{ leading: 'ps-1' }"
