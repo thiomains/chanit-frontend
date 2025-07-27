35d1ff6832f6377ba41d61f3ea209d9c8c797a00
thiomains <thiomains@icloud.com>
2025-07-28 01:41:41 +0200

    added reading messages

---
+++ app/components/TextMessageComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps(["message"])
+const message = props.message
+
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <UAvatar src="https://images.dog.ceo/breeds/doberman/n02107142_5570.jpg" size="xl" />
+    <div>
+      <p class="font-bold">{{ message.author }}</p>
+      <p>{{ message.body }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/channel/[id].vue
+++ app/pages/channel/[id].vue
@@ -1,49 +1,80 @@
 <script setup>
+definePageMeta({
+  layout: 'sidebar'
+})
 
 const config = useRuntimeConfig()
 const route = useRoute()
 const session = useState('session')
-const { $checkToken } = useNuxtApp()
+const {$checkToken} = useNuxtApp()
 
 onMounted(async () => {
 
   try {
-    sose.value = await $fetch(config.public.apiBaseUrl + "/channel/" + route.params.id, {
+    const res = await $fetch(config.public.apiBaseUrl + "/channel/" + route.params.id, {
       method: "GET",
       headers: {
         Authorization: `Bearer ${session.value.session.accessToken}`,
         Session: session.value.session.sessionId
       }
     })
+
+    if (res.channel.channelType === "direct-message") {
+      let members = res.channel.directMessageChannel.members
+      members.splice(members.indexOf(session.value.user))
+      channelName.value = members[0]
+    }
+
   } catch (e) {
     console.log(e)
   }
 
-  try {
-    messages.value = await $fetch(config.public.apiBaseUrl + "/channel/" + route.params.id + "/messages", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${session.value.session.accessToken}`,
-        Session: session.value.session.sessionId
-      }
-    })
-  } catch (e) {
-    console.log(e)
-  }
+  fetchMessages()
 
 })
 
-let sose = ref('')
+async function fetchMessages() {
+  try {
+    const res = await $fetch(config.public.apiBaseUrl + "/channel/" + route.params.id + "/messages", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${session.value.session.accessToken}`,
+        Session: session.value.session.sessionId
+      }
+    })
+    messages.value = res
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+let channelName = ref('')
 
 let messages = ref([])
+
+async function messageSent(message) {
+  message.pending = true;
+  messages.value.push(message)
+  await fetchMessages()
+}
 
 </script>
 
 <template>
-  <div class="size-full">
-    <p>{{ sose }}</p>
+  <div class="p-2">
+    <UCard variant="subtle">
+      <div class="flex items-center gap-2">
+        <UAvatar src="https://images.dog.ceo/breeds/puggle/IMG_075018.jpg" />
+        <p class="font-bold">{{ channelName }}</p>
+      </div>
+    </UCard>
+    <div class="flex flex-col gap-2 my-4 overflow-y-scroll">
+      <div v-for="message in messages">
+        <TextMessageComponent :message="message" />
+      </div>
+    </div>
+    <TextChannelInputComponent @message-sent="messageSent" />
   </div>
-  <TextChannelInputComponent />
 </template>
 
 <style scoped>
