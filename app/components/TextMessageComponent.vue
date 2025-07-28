1782d37231a09ad3b573ba438fe985c198568237
thiomains <thiomains@icloud.com>
2025-07-28 02:51:23 +0200

    added username of message author

--- app/components/TextMessageComponent.vue
+++ app/components/TextMessageComponent.vue
@@ -8,7 +8,7 @@ const message = props.message
   <div class="flex gap-2">
     <UAvatar src="https://images.dog.ceo/breeds/doberman/n02107142_5570.jpg" size="xl" />
     <div>
-      <p class="font-bold">{{ message.author }}</p>
+      <p class="font-bold">{{ message.author.username }}</p>
       <p>{{ message.body }}</p>
     </div>
   </div>
