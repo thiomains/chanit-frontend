e6a85cbad100e78201700237cd97626d151efcaa
thiomains <thiomains@icloud.com>
2025-07-30 02:13:47 +0200

    added typing indicators

--- app/components/TextChannelInputComponent.vue
+++ app/components/TextChannelInputComponent.vue
@@ -2,6 +2,7 @@
 const config = useRuntimeConfig()
 const route = useRoute()
 const session = useState("session")
+const { $connectWebsocket } = useNuxtApp()
 
 const emit = defineEmits(['message-sent'])
 
@@ -31,11 +32,45 @@ async function enterPressed() {
     console.log(e)
   }
 }
+
+onMounted(async () => {
+  const ws = await $connectWebsocket()
+  ws.addEventListener("message", (event) => {
+    const message = JSON.parse(event.data)
+    if (!message) return
+    if (message.type === "message") showTyping.value = false
+    if (message.type !== "typing") return
+    if (message.channelId !== route.params.id) return
+    if (message.userId === session.value.user.userId) return
+    lastTyping.value = Date.now()
+    showTyping.value = true
+    setTimeout(() => {
+      if (lastTyping.value < Date.now() - 3000) showTyping.value = false
+    }, 4000)
+  })
+})
+
+async function sendTyping() {
+  const ws = await $connectWebsocket()
+  ws.send(JSON.stringify({
+    type: "typing",
+    channelId: route.params.id
+  }))
+}
+
+let lastTyping = ref(0)
+let showTyping = ref(false)
+let typingUser = ref({})
+
 </script>
 
 <template>
+  <div class="h-4 px-6 mt-2">
+    <p v-if="showTyping" ><b class="font-bold">{{ typingUser }}</b> is typing...</p>
+  </div>
   <div class="p-4">
     <UInput
+        @input="sendTyping"
         @keydown.enter="enterPressed"
         class="w-full"
         v-model="messageInput"
