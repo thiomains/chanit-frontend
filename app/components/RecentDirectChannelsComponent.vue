9d34ceb5cf9c53c38c988bc60c999da520f1ed19
thiomains <thiomains@icloud.com>
2025-07-30 05:12:47 +0200

    added recent direct channels section

---
+++ app/components/RecentDirectChannelsComponent.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+const sessionState = useState('session')
+const config = useRuntimeConfig()
+
+interface RecentChannel {
+  channelId: string,
+  lastMessage?: {
+    channelId: string
+    createdAt: number,
+    messageId: string,
+    body: string,
+    author: {
+      username: string,
+    }
+  },
+  directMessageChannel: {
+    name?: string
+  }
+}
+
+async function loadRecentChannels() {
+  const session = sessionState.value as any
+
+  const res = await $fetch(config.public.apiBaseUrl + "/user/me/recent", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${session.session.accessToken}`,
+      Session: session.session.sessionId
+    }
+  })
+
+  data.value = res as RecentChannel[]
+  loaded.value = true
+}
+
+onMounted(async () => {
+  await loadRecentChannels()
+})
+
+function subtitle(channel: RecentChannel) {
+  if (!channel.lastMessage) return ""
+  let text = channel.lastMessage.author.username + ": "
+  text = text + channel.lastMessage.body
+  text = text.substring(0, 28)
+  text = text + "..."
+  return text
+}
+
+let data = ref<RecentChannel[]>([])
+let loaded = ref(false)
+
+</script>
+
+<template>
+  <div v-if="loaded">
+    <UButton
+        class="flex-row text-start items-center justify-start"
+        v-for="channel of data"
+        :key="channel.lastMessage?.messageId || channel.channelId"
+        :to="'/channel/' + channel.channelId"
+        active-variant="subtle"
+        variant="ghost"
+        color="neutral"
+        block
+    >
+      <UAvatar src="https://images.dog.ceo/breeds/terrier-fox/n02095314_464.jpg" size="lg"/>
+      <div>
+        <p>{{ channel.directMessageChannel.name || channel.channelId }}</p>
+        <p class="text-muted">{{ subtitle(channel) }}</p>
+      </div>
+    </UButton>
+  </div>
+  <div v-else>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- app/components/SidebarChannelSelectComponent.vue
+++ app/components/SidebarChannelSelectComponent.vue
@@ -7,9 +7,13 @@
     <div class="flex flex-col gap-2 mb-4">
       <UButton size="xl" to="/" label="Friends" active-variant="subtle" color="neutral" variant="ghost" icon="material-symbols:group"/>
       <UButton size="xl" to="/test" label="Test" active-variant="subtle" color="neutral" variant="ghost" icon="material-symbols:question-mark"/>
+      <UButton size="xl" to="/logout" label="Log out" active-variant="subtle" color="neutral" variant="ghost" icon="material-symbols:exit-to-app"/>
     </div>
     <USeparator/>
-
+    <div class="flex flex-col gap-2 mb-4">
+      <h3 class="text-dimmed mt-4 font-bold">Recent Direct Messages</h3>
+        <RecentDirectChannelsComponent />
+    </div>
   </UCard>
 </template>
 
