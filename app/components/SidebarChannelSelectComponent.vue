058d0933579f5b6bdec53a66ddabf22220040b67
thiomains <thiomains@icloud.com>
2025-08-15 22:33:25 +0200

    removed test page and corresponding button in SidebarChannelSelectComponent

--- app/components/SidebarChannelSelectComponent.vue
+++ app/components/SidebarChannelSelectComponent.vue
@@ -6,7 +6,6 @@
   <UCard class="w-full grow">
     <div class="flex flex-col gap-2 mb-4">
       <UButton size="xl" to="/" label="Friends" active-variant="subtle" color="neutral" variant="ghost" icon="material-symbols:group"/>
-      <UButton size="xl" to="/test" label="Test" active-variant="subtle" color="neutral" variant="ghost" icon="material-symbols:question-mark"/>
       <UButton size="xl" to="/logout" label="Log out" active-variant="subtle" color="neutral" variant="ghost" icon="material-symbols:exit-to-app"/>
     </div>
     <USeparator/>
