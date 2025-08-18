a59c4db04fe275ca4e57c98fcfcf6d356fd61e8a
thiomains <thiomains@icloud.com>
2025-08-18 23:57:03 +0200

    added support for notification body and silent mode in `NotificationSoundComponent.vue`

--- app/components/NotificationSoundComponent.vue
+++ app/components/NotificationSoundComponent.vue
@@ -12,14 +12,18 @@ onMounted(async () => {
     const message = JSON.parse(event.data)
     if (message.type !== "notification") return
     audio.play()
+    const options = {
+      body: message.content.body,
+      silent: true
+    }
     if (!("Notification" in window)) {
       alert("This browser does not support desktop notification");
     } else if (Notification.permission === "granted") {
-      const notification = new Notification("Hi there!");
+      const notification = new Notification("Hi there!", options);
     } else if (Notification.permission !== "denied") {
       Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
-          const notification = new Notification("Hi there!");
+          const notification = new Notification("Hi there!", options);
         }
       });
     }
