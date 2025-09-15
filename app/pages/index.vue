1a821b2313a71720f9fb67d471a5c2748cf764cd
thiomains <thiomains@icloud.com>
2025-09-15 22:27:38 +0200

    fix: redirect to /app on index page load to prevent 404 error

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+ onMounted(async () => {
+   await navigateTo({
+     path: "/app"
+   })
+   window.location.reload()
+ })
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
